d6551c07a135a06b5e72aa9ddf32195223bd55e1
slyty7 <tyler.j.west.7@gmail.com>
2017-08-31 12:43:24 -0700

    add am and pm

--- javascript/main.js
+++ javascript/main.js
@@ -1,8 +1,11 @@
 //invokes functions as soon as window loads
 window.onload = function(){
 	clock();
+	ampm();
 	setInterval(clock, 1000);
+	setInterval(ampm, 1000);
 };
+
 
 //gets current time and changes html to reflect it
 function clock(){
@@ -10,7 +13,6 @@ function clock(){
 	var hours = date.getHours();
 	var minutes = date.getMinutes();
 	var seconds = date.getSeconds();
-	var meridian = (hours >= 12) ? 'pm' : 'am';
 
 	//make clock a 12 hour clock instead of 24 hour clock
 	hours = (hours > 12) ? (hours - 12) : hours;
@@ -24,13 +26,24 @@ function clock(){
 	document.getElementsByClassName('hours')[0].innerHTML = hours;
 	document.getElementsByClassName('minutes')[0].innerHTML = minutes;
 	document.getElementsByClassName('seconds')[0].innerHTML = seconds;
-	document.getElementsByClassName('meridian')[0].innerHTML = meridian;
 	
 }
 
 //turns single digit numbers to two digit numbers by placing a zero in front
 function addZero (val){
-	return (val <= 9 && "0" + val) || val;
+	return (val <= 9) ? ("0" + val) : val;
 }
 
+//highlights either am or pm on clock
+function ampm(){
+	var date = new Date();
+	var hours = date.getHours();
+	var am = document.getElementsByClassName("am")[0].classList;
+	var pm = document.getElementsByClassName("pm")[0].classList;
+	
 
+	(hours >= 12) ? pm.add("light-on") : am.add("light-on");
+	(hours >= 12) ? am.remove("light-on") : pm.remove("light-on");
+}
+
+//highlights what day of the week it is
