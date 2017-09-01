30810d74126e68728c3b1ed32b0fb3646189be3b
slyty7 <tyler.j.west.7@gmail.com>
2017-08-31 20:02:27 -0700

    add functionalityto days of week

--- javascript/main.js
+++ javascript/main.js
@@ -1,18 +1,22 @@
 //invokes functions as soon as window loads
 window.onload = function(){
-	clock();
+	time();
 	ampm();
-	setInterval(clock, 1000);
-	setInterval(ampm, 1000);
+	whatDay();
+	setInterval(function(){
+		time();
+		ampm();
+		whatDay();
+	}, 1000);
 };
 
 
 //gets current time and changes html to reflect it
-function clock(){
-	var date = new Date();
-	var hours = date.getHours();
-	var minutes = date.getMinutes();
-	var seconds = date.getSeconds();
+function time(){
+	var date = new Date(),
+		hours = date.getHours(),
+		minutes = date.getMinutes(),
+		seconds = date.getSeconds();
 
 	//make clock a 12 hour clock instead of 24 hour clock
 	hours = (hours > 12) ? (hours - 12) : hours;
@@ -34,16 +38,37 @@ function addZero (val){
 	return (val <= 9) ? ("0" + val) : val;
 }
 
-//highlights either am or pm on clock
+//lights up either am or pm on clock
 function ampm(){
-	var date = new Date();
-	var hours = date.getHours();
-	var am = document.getElementsByClassName("am")[0].classList;
-	var pm = document.getElementsByClassName("pm")[0].classList;
+	var date = new Date(),
+		hours = date.getHours(),
+		am = document.getElementsByClassName("am")[0].classList,
+		pm = document.getElementsByClassName("pm")[0].classList;
 	
 
 	(hours >= 12) ? pm.add("light-on") : am.add("light-on");
 	(hours >= 12) ? am.remove("light-on") : pm.remove("light-on");
 }
 
-//highlights what day of the week it is
+//lights up what day of the week it is
+function whatDay(){
+	var date = new Date();
+	var currentDay = date.getDay();
+	var days = {
+		0: "sunday",
+		1: "monday",
+		2: "tuesday",
+		3: "wednesday",
+		4: "thursday",
+		5: "friday",
+		6: "saturday"
+	}
+	var currentDayHTML = document.getElementsByClassName(days[currentDay])[0].innerHTML;
+	var currentDayClass = document.getElementsByClassName(days[currentDay])[0].classList;
+	var previousDayClass = document.getElementsByClassName(days[currentDay-1])[0].classList;
+
+	//not quite right.  doesnt remove light on Saturday to Sunday switch due to days array
+	currentDayClass.add("light-on");
+	previousDayClass.remove("light-on");
+
+}
