c98ab2d800a81ea1f83368ce646f91c0315fc1c7
Tyler West <tyler.j.west.7@gmail.com>
2020-07-02 11:59:39 -0700

    update and sanitize

--- javascript/main.js
+++ javascript/main.js
@@ -1,66 +1,72 @@
-//invokes functions as soon as window loads
-window.onload = function(){
-	time();
-	ampm();
-	whatDay();
-	setInterval(function(){
-		time();
-		ampm();
-		whatDay();
-	}, 1000);
-};
+(() => {
+	const init = () => {
+		// on window loading
+		window.onload = () => {
+			time();
+			ampm();
+			whatDay();
+			setInterval(() => {
+				time();
+				ampm();
+				whatDay();
+			}, 1000);
+		};
+	};
 
+	//gets current time and changes html to reflect it
+	const time = () => {
+		let date = new Date();
+		let	hours = date.getHours();
+		let	minutes = date.getMinutes();
+		let	seconds = date.getSeconds();
 
-//gets current time and changes html to reflect it
-function time(){
-	var date = new Date(),
-		hours = date.getHours(),
-		minutes = date.getMinutes(),
-		seconds = date.getSeconds();
+		//make clock a 12 hour clock instead of 24 hour clock
+		hours = (hours > 12) ? (hours - 12) : hours;
+		hours = (hours === 0) ? 12 : hours;
 
-	//make clock a 12 hour clock instead of 24 hour clock
-	hours = (hours > 12) ? (hours - 12) : hours;
-	hours = (hours === 0) ? 12 : hours;
+		//invokes function to make sure number has at least two digits
+		hours = addZero(hours);
+		minutes = addZero(minutes);
+		seconds = addZero(seconds);
 
-	//invokes function to make sure number has at least two digits
-	hours = addZero(hours);
-	minutes = addZero(minutes);
-	seconds = addZero(seconds);
-
-	//changes the html to match results
-	document.getElementsByClassName('hours')[0].innerHTML = hours;
-	document.getElementsByClassName('minutes')[0].innerHTML = minutes;
-	document.getElementsByClassName('seconds')[0].innerHTML = seconds;
-}
-
-//turns single digit numbers to two digit numbers by placing a zero in front
-function addZero (val){
-	return (val <= 9) ? ("0" + val) : val;
-}
-
-//lights up either am or pm on clock
-function ampm(){
-	var date = new Date(),
-		hours = date.getHours(),
-		am = document.getElementsByClassName("am")[0].classList,
-		pm = document.getElementsByClassName("pm")[0].classList;
-	
-		
-	(hours >= 12) ? pm.add("light-on") : am.add("light-on");
-	(hours >= 12) ? am.remove("light-on") : pm.remove("light-on");
-}
-
-//lights up what day of the week it is
-function whatDay(){
-	var date = new Date(),
-		currentDay = date.getDay(),
-		days = document.getElementsByClassName("day");
-
-	//iterates through all divs with a class of "day"
-	for (x in days){
-		//list of classes in current div
-		var classArr = days[x].classList;
-
-		(classArr !== undefined) && ((x == currentDay) ? classArr.add("light-on") : classArr.remove("light-on"));
+		//changes the html to match results
+		document.getElementsByClassName('hours')[0].innerHTML = hours;
+		document.getElementsByClassName('minutes')[0].innerHTML = minutes;
+		document.getElementsByClassName('seconds')[0].innerHTML = seconds;
 	}
-}
+
+	//turns single digit numbers to two digit numbers by placing a zero in front
+	const addZero = val => {
+		return (val <= 9) ? ("0" + val) : val;
+	}
+
+	//lights up either am or pm on clock
+	const ampm = () => {
+		let date = new Date();
+		let	hours = date.getHours();
+		let	am = document.getElementsByClassName("am")[0].classList;
+		let	pm = document.getElementsByClassName("pm")[0].classList;
+
+
+		(hours >= 12) ? pm.add("light-on") : am.add("light-on");
+		(hours >= 12) ? am.remove("light-on") : pm.remove("light-on");
+	}
+
+	//lights up what day of the week it is
+	const whatDay = () => {
+		let date = new Date();
+		let	currentDay = date.getDay();
+		let	days = document.getElementsByClassName("day");
+
+		//iterates through all divs with a class of "day"
+		for (x in days){
+			//list of classes in current div
+			var classArr = days[x].classList;
+
+			(classArr !== undefined) && ((x == currentDay) ? classArr.add("light-on") : classArr.remove("light-on"));
+		}
+	}
+
+
+	return init();
+})()
