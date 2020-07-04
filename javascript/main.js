f4bd7eb62d6e58c01060048eb1c72a0f6093dcfc
Tyler West <tyler.j.west.7@gmail.com>
2020-07-04 10:11:02 -0700

    add date functionality

--- javascript/main.js
+++ javascript/main.js
@@ -13,16 +13,22 @@
 	};
 
 	const getCurrentTime = () => {
-		let date = new Date();
-		let day = date.getDay();
-		let	hours = date.getHours();
-		let	minutes = date.getMinutes();
-		let	seconds = date.getSeconds();
-		let period = (hours >= 12) ? "pm" : "am";		
+		let fullDate = new Date();
+		let month = fullDate.getMonth();
+		let date = fullDate.getDate();
+		let year = fullDate.getFullYear();
+		let day = fullDate.getDay();
+		let	hours = fullDate.getHours();
+		let	minutes = fullDate.getMinutes();
+		let	seconds = fullDate.getSeconds();
+		let period = (hours >= 12) ? "pm" : "am";
 
 		window.clock = {};
 		window.clock.time = {
+			fullDate: fullDate,
+			month: month,
 			date: date,
+			year: year,
 			day: day,
 			hours: hours,
 			minutes: minutes,
@@ -33,6 +39,9 @@
 
 	const displayCurrentTime = () => {
 		let day = window.clock.time.day;
+		let month = window.clock.time.month;
+		let date = window.clock.time.date;
+		let year = window.clock.time.year;
 		let hours = window.clock.time.hours;
 		let minutes = window.clock.time.minutes;
 		let seconds = window.clock.time.seconds;
@@ -46,20 +55,37 @@
 		minutes = (minutes <= 9) ? ("0" + minutes) : minutes;
 		//formats seconds
 		seconds = (seconds <= 9) ? ("0" + seconds) : seconds;
+		//formats day
+		let days = ["sunday","monday","tuesday","wednesday","thursday","friday","saturday"];
+		let dayText = ``;
+		if(days[day].length === 6) dayText = `<span style="color: #2b2828;">ooo</span>${days[day]}`;
+		if(days[day].length === 7) dayText = `<span style="color: #2b2828;">oo</span>${days[day]}`;
+		if(days[day].length === 8) dayText = `<span style="color: #2b2828;">o</span>${days[day]}`;
+		if(days[day].length === 9) dayText = `<span style="color: #2b2828;"></span>${days[day]}`;
+		//formats month
+		let months = ["jan","feb","mar","apr","may","jun","jul","aug","sep","oct","nov","dec"];
+		//formats date
+		date = (date <= 9) ? ("0" + date) : date;
 
 		// targets the html
+		const dayHtml = document.getElementsByClassName('day-alpha')[0];
+		const monthHtml = document.getElementsByClassName('month-alpha')[0];
+		const dateHtml = document.getElementsByClassName('date-number')[0];
+		const yearHtml = document.getElementsByClassName('year-number')[0];
 		const hoursHtml = document.getElementsByClassName('hours')[0];
 		const minutesHtml = document.getElementsByClassName('minutes')[0];
 		const secondsHtml = document.getElementsByClassName('seconds')[0];
 		const periodHtml = document.getElementsByClassName(period)[0];
-		const dayHtml = document.getElementsByClassName("day")[day];
 
-		//changes the html values
+		// changes the html values
+		dayHtml.innerHTML = dayText;
+		monthHtml.innerHTML = months[month];
+		dateHtml.innerHTML = date;
+		yearHtml.innerHTML = year;
 		hoursHtml.innerHTML = hours;
 		minutesHtml.innerHTML = minutes;
 		secondsHtml.innerHTML = seconds;
 		periodHtml.classList.add("light-on");
-		dayHtml.classList.add("light-on");
 	};
 
 	const resetClock = () => {
